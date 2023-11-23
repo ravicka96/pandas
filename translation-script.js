52bdbb3ceca06be541df5c6aa10315b2f5a35555
ravicka96 <kavya.ravi96@outlook.com>
2023-11-23 20:17:56 +0530

    updated section locator

--- translation-script.js
+++ translation-script.js
@@ -6,8 +6,8 @@ function applyTranslations(translation) {
   document.getElementById('appearance').querySelector('h2').textContent = translation.appearance;
   document.getElementById('habitat').querySelector('h2').textContent = translation.habitat;
   document.getElementById('diet').querySelector('h2').textContent = translation.diet;
-  document.getElementById('solitary-nature').querySelector('h2').textContent = translation.solitary_nature;
-  document.getElementById('survival-threats').querySelector('h2').textContent = translation.survival_threats;
+  document.getElementById('solitary_nature').querySelector('h2').textContent = translation.solitary_nature;
+  document.getElementById('survival_threats').querySelector('h2').textContent = translation.survival_threats;
 
   // Apply translations to navigation elements as needed
   document.getElementById('introduction_p').textContent = translation.introduction;
@@ -22,8 +22,8 @@ function applyTranslations(translation) {
   document.getElementById('appearance').querySelector('p').textContent = translation.appearance_p;
   document.getElementById('habitat').querySelector('p').textContent = translation.habitat_p;
   document.getElementById('diet').querySelector('p').textContent = translation.diet_p;
-  document.getElementById('solitary-nature').querySelector('p').textContent = translation.solitary_nature_p;
-  document.getElementById('survival-threats').querySelector('p').textContent = translation.survival_threats_p;
+  document.getElementById('solitary_nature').querySelector('p').textContent = translation.solitary_nature_p;
+  document.getElementById('survival_threats').querySelector('p').textContent = translation.survival_threats_p;
 
   // Apply translations to copyright element as needed
   document.getElementById('footer').textContent = translation.copyright;
